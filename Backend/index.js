c0d5a9f454816527059fd1f977fdfee4ba8f529a
Aree-Vanier <git@gregk.ca>
2020-11-07 17:32:38 -0500

    Basic rough JSON-based system built

--- Backend/index.js
+++ Backend/index.js
@@ -1,14 +1,16 @@
 const express = require("express");
-var bodyParser = require('body-parser')
+var bodyParser = require('body-parser');
+const e = require("express");
 var app = express();
 
 // create application/x-www-form-urlencoded parser
 var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
-
 var totalScore = 0;
 var nextMilestone = 1000;
 var leaderboard = new Map()
+/** {@type {id:string, name:string, score:number}[]} */
+var top = [{id:"0", name:"0", score:0}];
 
 var server = app.listen(2708, ()=>{
     console.log(`Running on ${server.address().address}:${server.address().port}`);
@@ -33,12 +35,33 @@ app.post("/addScore", urlencodedParser, (req, res) =>{
         totalScore -= existing.score*1;
     }
 
-    leaderboard.set(id, {id, name:req.body.name, score:req.body.score})
-
+    
+    // If the score has increased
+    if((existing != null ? existing.score : 0)< req.body.score) {
+        // Add to leaderboard
+        leaderboard.set(id, {id, name:req.body.name, score:req.body.score});
+        // Variable to prevent multiple inserts
+        let inserted = false;
+        // Update top 10 if necessary
+        for(en of top){
+            console.log(en);
+            if(req.body.score > en.score && !inserted){
+                top.splice(top.indexOf(en), 0, {id, name:req.body.name, score:req.body.score});
+                inserted = true;
+            }
+            if(en.id == req.body.id){
+                top.splice(top.indexOf(en), 1);
+            }
+        }
+        // Keep at 10 entries
+        if(top.length > 10){
+            top.pop()
+        }
+    }
 
     totalScore += req.body.score*1;
 
-    res.json({totalScore, nextMilestone});
+    res.json({totalScore, nextMilestone, top});
 })
 
 
