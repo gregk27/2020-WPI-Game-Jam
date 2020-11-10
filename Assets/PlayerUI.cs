9425cf9006bbb3db50b217f1f8c9b50bba075b48
x3a1n4 <billybobbyjoeyyay@gmail.com>
2020-11-09 23:52:39 -0500

    Added some UI elements to be used in-game

---
+++ Assets/PlayerUI.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerUI : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject mainUI;
+    public GameObject firstTowerUI;
+    public GameObject buyTowerUI;
+    public GameObject onTowerUI;
+
+    void Start()
+    {
+        //hide all the UI's
+        firstTowerUI.SetActive(false);
+        buyTowerUI.SetActive(false);
+        onTowerUI.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
