6fb0b196ecb31b1b8cdd09aa1d7cdba0b2716353
gregk27 <git@gregk.ca>
2020-11-13 21:00:06 -0500

    Added script to get server status

---
+++ Assets/Scripts/Backend/Backend.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public static class Backend
+{
+
+private static readonly string serverAddress = "http://server.lan:2708";
+    private static readonly string getStatus = "/status";
+
+    public static bool GetStatus()
+    {
+        HttpWebRequest req = (HttpWebRequest)WebRequest.Create(serverAddress + getStatus);
+        HttpWebResponse res = (HttpWebResponse)req.GetResponse();
+
+        StreamReader reader = new StreamReader(res.GetResponseStream());
+        string resString = reader.ReadToEnd();
+        return resString.Equals("Alive");
+    }
+
+
+}
