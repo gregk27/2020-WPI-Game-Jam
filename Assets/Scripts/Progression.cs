d9e43cf7f43354903d6230364358d61283e16c9d
gregk27 <git@gregk.ca>
2020-11-14 22:16:02 -0500

    Added code to read JSON file

---
+++ Assets/Scripts/Progression.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Progression
+{
+    
+    [Serializable]
+    public struct Unlock {
+        public int baseScore;
+        public string name;
+        public string desc;
+        public float scaleFactor;
+        public string type;
+        public string id;
+        public int value;
+    }
+
+    [Serializable]
+    public struct ProgressionData
+    {
+        public List<Unlock> unlocks;
+    }
+
+    public static List<Unlock> unlocks { get; private set; }
+
+
+    static Progression()
+    {
+        Debug.Log("Initialising");
+        TextAsset jsonFile = Resources.Load<TextAsset>("Progression");
+        unlocks = JsonUtility.FromJson<ProgressionData>(jsonFile.text).unlocks;
+    }
+
+    
+}
---
+++ Assets/Scripts/ProgressionTest.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProgressionTest : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Hello World!");
+        Debug.Log(Progression.unlocks[0].name);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
