e6c3a95914e17918d62a0b85e1c3d0ba75548d08
x3a1n4 <billybobbyjoeyyay@gmail.com>
2020-11-14 20:58:46 -0500

    Added some UI elements

---
+++ Assets/BuyTowerUIScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BuyTowerUIScript : MonoBehaviour
+{
+    public List<Sprite> towerIcons;
+    public Text towerName;
+    public Image towerImage;
+    public Text towerCost;
+    private int thisTower = 0;
+
+    // Start is called before the first frame update
+    void Start() {
+        
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+
+    public void NextTower() {
+
+    }
+
+    public void PreviousTower() {
+
+    }
+}
--- Assets/Scripts/PlayerUI.cs
+++ Assets/Scripts/PlayerUI.cs
@@ -12,6 +12,8 @@ public class PlayerUI : MonoBehaviour
     public GameObject deathScreenUI;
     public GameObject startScreenUI;
 
+    private List<CanvasGroup> uiList = new List<CanvasGroup>();
+
     private Vector3 startPos;
 
     //also do particles here, why not
@@ -22,18 +24,31 @@ public class PlayerUI : MonoBehaviour
     //private PlayerController playerController;
     void HideUI() {
         //hide all the UI's
-        firstTowerUI.SetActive(false);
-        buyTowerUI.SetActive(false);
-        onTowerUI.SetActive(false);
-        mainUI.SetActive(false);
-        deathScreenUI.SetActive(false);
-        startScreenUI.SetActive(false);
+        foreach(CanvasGroup ui in uiList) {
+            ui.interactable = false;
+            ui.alpha = 0;
+            ui.blocksRaycasts = false;
+        }
+    }
+
+    static void ShowUI(GameObject g) {
+        CanvasGroup c = g.GetComponent<CanvasGroup>();
+        c.interactable = true;
+        c.alpha = 1;
+        c.blocksRaycasts = true;
     }
 
     void Start()
     {
+        uiList.Add(firstTowerUI.GetComponent<CanvasGroup>());
+        uiList.Add(buyTowerUI.GetComponent<CanvasGroup>());
+        uiList.Add(onTowerUI.GetComponent<CanvasGroup>());
+        uiList.Add(mainUI.GetComponent<CanvasGroup>());
+        uiList.Add(deathScreenUI.GetComponent<CanvasGroup>());
+        uiList.Add(startScreenUI.GetComponent<CanvasGroup>());
+
         HideUI();
-        startScreenUI.SetActive(true);
+        ShowUI(startScreenUI);
 
         particleSystem = GetComponentInChildren<ParticleSystem>();
 
@@ -45,12 +60,6 @@ public class PlayerUI : MonoBehaviour
         startPos = transform.position;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     //called when game start button is clicked
     public void StartButtonClicked() {
         //burst of particles
@@ -59,17 +68,20 @@ public class PlayerUI : MonoBehaviour
         GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Dynamic;
         gameObject.GetComponent<SpriteRenderer>().color = new Color(1, 1, 1, 1);
         transform.position = startPos;
+
+        //hide UI
         HideUI();
-        mainUI.SetActive(true);
+        ShowUI(mainUI);
+
         EnemySpawner.instance.ResetEnemies();
         EnemySpawner.instance.gameIsPlaying = true;
 
-        
+        firstTowerUI.GetComponent<FirstTowerUIScript>().StartTimer();
     }
 
     public void PlayerDied() {
         HideUI();
-        deathScreenUI.SetActive(true);
+        ShowUI(deathScreenUI);
         EnemySpawner.instance.gameIsPlaying = false;
         
         particleSystem.Play();
